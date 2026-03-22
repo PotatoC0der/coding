64db62b4099ecd5588ea638bfb1da25ec4380681
PotatoC0der <161310362+PotatoC0der@users.noreply.github.com>
2026-03-22 17:15:57 +0800

    Add files via upload

---
+++ number_guesser_gordon_ramsay_edition/number_guesser.cs
@@ -0,0 +1,38 @@
+﻿Console.Write("User 1, enter a number between 0 and 100: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0 && number > 100)
+{
+    Console.WriteLine("Read the instruction again and pick another number!");
+}
+else 
+{
+    Console.WriteLine("Number logged, proceeding to next phase of program");
+    Console.Clear();
+}
+Console.Write("User 2, guess the number: ");
+int answer = Convert.ToInt32(Console.ReadLine());
+if (answer != number)
+{
+    Console.WriteLine("Number is wrong.");
+    while (answer != number)
+    {
+        Console.Write("What is your next guess? ");
+        answer = Convert.ToInt32(Console.ReadLine());
+        if (answer < number)
+        {
+            Console.WriteLine($"{answer} is too low!");
+        }
+        else if (answer > number)
+        {
+            Console.WriteLine($"{answer} is too high!");
+        }
+        else
+        {
+            Console.WriteLine($"{answer} is correct!");
+        }
+    }
+}
+else
+{
+    Console.WriteLine($"{answer} is correct!");
+}
---
+++ number_guesser_gordon_ramsay_edition/number_guesser_gordon_ramsay_edition/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine($"*In Gordon Ramsay's voice*");
+Console.WriteLine("For our challenge today each chef will designate a number while the other team will try to guess it.");
+Console.WriteLine("Blue team, you will guess what the Red team's number is.");
+Console.Write("Red team, your number please: ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number < 0 || number > 100)
+{
+    Console.Write("Again Red team, your number please: ");
+    number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("YOU FUCKING DONKEY! Start again.");
+}
+Console.Clear();
+Console.Write("Alright Blue team, now try to guess the number: ");
+int answer = Convert.ToInt32(Console.ReadLine());
+if (answer != number)
+{
+    Console.WriteLine("Number is wrong.");
+    while (answer != number)
+    {
+        Console.Write("What is your next guess? ");
+        answer = Convert.ToInt32(Console.ReadLine());
+        if (answer < number)
+        {
+            Console.WriteLine($"{answer} is raw it can still fly that pigeon!");
+        }
+        else if (answer > number)
+        {
+            Console.WriteLine($"{answer} is way too fucking overcooked!");
+        }
+        else
+        {
+            Console.WriteLine($"{answer} is correct!");
+            Console.WriteLine("Press any key to quit.");
+            Console.ReadKey();
+        }
+    }
+}
+else
+{
+    Console.WriteLine($"{answer} is correct!");
+    Console.WriteLine("Press any key to quit.");
+    Console.ReadKey();
+}
