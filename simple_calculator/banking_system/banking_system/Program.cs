e1b8b7c5bebff434cccb0cfee31f2149edbb9108
PotatoC0der <161310362+PotatoC0der@users.noreply.github.com>
2026-03-22 17:16:58 +0800

    Add files via upload

---
+++ simple_calculator/banking_system/banking_system/Program.cs
@@ -0,0 +1,67 @@
+﻿Console.Title = "Bel Bank";
+
+int userInput;
+string userName;
+string password;
+
+Greeting();
+Evaluation();
+
+void Loading()
+{
+    Console.WriteLine("=============================================================================");
+}
+
+void Greeting()
+{
+    Console.WriteLine("Greetings and Welcome to the Bel Bank Banking System");
+    Console.WriteLine("What would you like to do today?");
+    Console.WriteLine("[1]  Create New Account");
+    Console.WriteLine("[2]  Login to Existing Account");
+    Console.WriteLine("[3]  Exit");
+    Console.Write("Select from the options above: ");
+    userInput = Convert.ToInt32(Console.ReadLine());    
+}
+
+void Evaluation()
+{
+    if (userInput == 3)
+    { 
+        Console.WriteLine("Thank you for using the Bel Banking System.");
+        Console.WriteLine("Have a good day.");
+        Loading();
+    }
+    while (userInput != 3)
+    {
+        if (userInput < 1 || userInput > 3)
+        {
+            Loading();
+            Console.WriteLine("Invalid command, try again.");
+            Loading();
+            Greeting();
+        }
+        else if (userInput > 0 || userInput < 4)
+        {
+            switch (userInput)
+            {
+                case 1:
+                    Loading(); 
+                    Console.WriteLine("Welcome to the Account Creation.");
+                    Console.WriteLine("Please input the necessary information.");
+                    Loading();
+                    break;
+                case 2:
+                    Loading();
+                    Console.WriteLine("Please input your username and password below:");
+                    Console.Write("Username: ");
+                    userName = Console.ReadLine();
+                    Console.Write("Password: ");
+                    password = Console.ReadLine();
+                    Loading();
+                    break;
+            }
+        }
+
+    }
+}
+
---
+++ simple_calculator/simple_calculator/simple_calculator/Program.cs
@@ -0,0 +1,110 @@
+﻿Console.Title = "Simple Calculator";
+int choice;
+float numOne;
+float numTwo;
+
+
+Console.WriteLine("Welcome to the Simple Calculator");
+Loading();
+Selector();
+Evaluator();
+Program();
+while (choice != 5)
+{
+    Selector();
+    Evaluator();
+    Program();
+}
+
+
+
+void Loading()
+{
+    Console.WriteLine("============================================================================");
+}
+
+void Selector()
+{
+    Console.WriteLine($"[1] Addition");
+    Console.WriteLine($"[2] Subtraction");
+    Console.WriteLine($"[3] Multiplication");
+    Console.WriteLine($"[4] Division");
+    Console.WriteLine($"[5] Quit");
+    Console.Write("Please select desired operation from the options above: ");
+    choice = Convert.ToInt32(Console.ReadLine());
+}
+
+void Evaluator()
+{
+    if (choice == 5)
+    {
+        Console.WriteLine("Terminating Program...");
+        Loading();
+    }
+    while (choice < 1 || choice > 5)
+    {
+        Console.WriteLine("Invalid input. Try again.");
+        Selector();
+        Loading();
+    }
+    switch (choice)
+    {
+        case 1:
+            Console.WriteLine("Operation selected: Addition");
+            Loading();
+            break;
+        case 2:
+            Console.WriteLine("Operation selected: Subtraction");
+            Loading();
+            break;
+        case 3:
+            Console.WriteLine("Operation selected: Multiplication");
+            Loading();
+            break;
+        case 4:
+            Console.WriteLine("Operation selected: Division");
+            Loading();
+            break;
+    }
+}
+
+void Program()
+{
+    switch (choice)
+    {
+        case 1:
+            UserNumberInput();
+            float sum = numOne + numTwo;
+            Console.WriteLine($"The result is: {sum}");
+            Loading();
+            break;
+        case 2:
+            UserNumberInput();
+            float difference = numOne - numTwo;
+            Console.WriteLine($"The result is: {difference}");
+            Loading();
+            break;
+        case 3:
+            UserNumberInput();
+            float product = numOne * numTwo;
+            Console.WriteLine($"The result is: {product}");
+            Loading();
+            break;
+        case 4:
+            UserNumberInput();
+            float quotient = numOne / numTwo;
+            Console.WriteLine($"The result is: {quotient}");
+            Loading();
+            break;
+    }
+
+    void UserNumberInput()
+    {
+        Console.Write("Input first number : ");
+        numOne = Convert.ToSingle(Console.ReadLine());
+        Console.Write("Input second number: ");
+        numTwo = Convert.ToSingle(Console.ReadLine());
+    }
+}
+
+
